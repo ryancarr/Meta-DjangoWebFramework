4f932014fd32b23befab2d434084ad5894ccbaf5
Ryan Carr <ryancarr@users.noreply.github.com>
2024-03-25 19:35:03 -0500

    Performed tasks in Week 2 labs

--- LittleLemon/myapp/urls.py
+++ LittleLemon/myapp/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('home/', views.home),
     path('showform/', views.showform),
     path('getform/', views.getform),
+    path('drinks/<str:drink_name>', views.drinks, name='drinks'),
+    path('aboutus/', views.about),
+    path('menu/', views.menu),
+    path('book/', views.book),
 ]
--- LittleLemon/myapp/views.py
+++ LittleLemon/myapp/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+def drinks(request, drink_name):
+    drink = {
+        'mocha': 'type of coffee',
+        'tea': 'type of beverage',
+        'lemonade': 'type of refreshment'
+    }
+    return HttpResponse(f'<h2> {drink_name} </h2> {drink[drink_name]}')
+
 def home(request):
     return HttpResponse('<h1> Welcome to Little Lemon </h1>')
+
+def about(request):
+    return HttpResponse(f'<h1> About us </h1>')
+
+def menu(request):
+    return HttpResponse(f'<h1> Menu </h1>')
+
+def book(request):
+    return HttpResponse(f'<h1> Make a booking </h1>')
 
 def showform(request):
     return render(request, 'form.html')
